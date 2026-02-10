8c4e59533bc1d546fca8ed5d958954e1cea0106f
empatherys <88014442+Retinazer@users.noreply.github.com>
2026-02-10 09:16:26 +0700

    Add files via upload
    broo, now it has gold and also fixed prior bugs

--- simplecoinflipcombat.py
+++ simplecoinflipcombat.py
@@ -2,10 +2,12 @@
 
 import random
 
+gold = 10
 health = 3
 healthEnemy = 3
 coin = ["Heads", "Tails"]
-print("Flip the coin!! (Type anything to flip)")
+print("You have", gold, "Gold")
+print("Flip the coin!! (type anything to flip)")
 flip = str(input())
 if flip == "y" or "yes" :
     flip = True
@@ -19,27 +21,30 @@ while flip == True:
         print(coinflip)
         print("You hit for 1 damage", healthEnemy)
     else :
-        health -= 1
         print(coinflip)
+        health -= 1
         print("You got hit for 1 damage", health)
         
     count += 1
     print("Turn", count)
-    print("Flip the coin!! (Type anything to flip)")
+    print("Flip the coin!! (type anything to flip)")
     flip = str(input())
     if flip == "y" or "yes" :
      flip = True
     
     if health == 1 :
+        gold -= 10
         print(coinflip)
         print("You died!!")
+        print("You lost 10 Gold!!", gold)
+        print("You now have", gold, "Gold")
         break
     elif healthEnemy == 1 :
+        gold += 10
         print(coinflip)
         print("You killed it!!")
+        print("You earned 10 Gold!!")
+        print("You now have", gold, "Gold")
         break
 
     
-
-
-
